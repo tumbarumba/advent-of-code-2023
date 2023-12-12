024fc2dc9f9f93fd164b01cb8aa4238f33e61a69
Joe Schmetzer <joe.schmetzer@penten.com>
2023-12-12 11:39:04 +1100

    Add function to extract first digit from string

--- day01/src/main/kotlin/com/exubero/aoc/day01/Day01.kt
+++ day01/src/main/kotlin/com/exubero/aoc/day01/Day01.kt
@@ -3,3 +3,9 @@ package com.exubero.aoc.day01
 fun main() {
     println("Hello, world!")
 }
+
+class Day01 {
+    fun getFirstDigit(data: String): Char {
+        return data.filter { it.isDigit() }.first()
+    }
+}
--- day01/src/test/kotlin/com/exubero/aoc/day01/Day01Test.kt
+++ day01/src/test/kotlin/com/exubero/aoc/day01/Day01Test.kt
@@ -3,9 +3,27 @@ package com.exubero.aoc.day01
 import org.junit.jupiter.api.Test
 
 import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.Arguments
+import org.junit.jupiter.params.provider.MethodSource
+import java.util.stream.Stream
 
 class Day01Test {
-    @Test fun testGetMessage() {
-        assertEquals("Hello, world!", null)
+    private val day01 = Day01()
+
+    companion object ExtractParameters {
+        @JvmStatic
+        fun extractArguments(): Stream<Arguments> =
+            Stream.of(
+                Arguments.of("two934seven1", '9', '1', 91),
+                Arguments.of("8825eightknfv", '8', '5', 85),
+                Arguments.of("4twofour", '4', '4', 44)
+            )
+    }
+
+    @ParameterizedTest
+    @MethodSource("extractArguments")
+    fun testExtractFirstDigit(data: String, firstDigit: Char, lastDigit: Char, value: Int) {
+        assertEquals(firstDigit, day01.getFirstDigit(data))
     }
 }
